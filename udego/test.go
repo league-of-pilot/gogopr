1b851aac025a2f3a63b882f0ad70d4048a6cdb2e
Danh Tran <danh2k@nexondv.com>
2024-06-27 17:43:27 +0700

    first try with pos internal to keep track

--- udego/test.go
+++ udego/test.go
@@ -1,24 +1,39 @@
 package udego
 
 import (
+	"fmt"
 	"io"
 )
 
 type demoRead struct {
 	content string
+	pos     int
 }
 type demoWrite struct{}
 
 func PlayReader() {
-	io.Copy(demoWrite{}, demoRead{
+	io.Copy(demoWrite{}, &demoRead{
 		content: "This demo some custom read implementation but may have bugs, check with GPT",
 	})
 }
 
-func (demoRead) Read(b []byte) (n int, err error) {
-	return len(b), nil
+func (p *demoRead) Read(b []byte) (n int, err error) {
+	l := len(p.content)
+	if p.pos >= l {
+		println("EOF")
+		return 0, io.EOF
+	}
+
+	for i := 0; i < l && i < len(b); i++ {
+		iter := p.content[i]
+		b[i] = iter
+		p.pos++
+	}
+
+	return p.pos, nil
 }
 
 func (demoWrite) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs), len(bs))
 	return len(bs), nil
 }
