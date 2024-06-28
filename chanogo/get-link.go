a89f077ab35f360a39f3b65c0eb61d41300529c6
Danh Tran <danh2k@nexondv.com>
2024-06-28 10:01:51 +0700

    v-85 loop though lens chan and setup receiver

--- chanogo/get-link.go
+++ chanogo/get-link.go
@@ -3,25 +3,30 @@ package chanogo
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func GetLink() []string {
 	links := []string{
 		"http://youtube.com",
-		"http://facebook.com",
+		// "http://facebook.com",
 		"http://stackoverflow.com",
 		"http://google.com",
 		"http://golang.org",
-		// "http://amazon.com",
+		"http://amazon.com",
 	}
 	return links
 }
 
+// go routine fireup seperately and init log does not comes in order
 func CheckLink(link string, c chan string, i int) {
-	var res string
+	println("init routine", i)
 	_, err := http.Get(link)
+
+	// Có thể gom style if-else res nhưng tách ra để dễ hack
+	var res string
 	if err != nil {
-		res = string(i) + " Error:" + err.Error()
+		res = strconv.Itoa(i) + " Error:" + err.Error()
 		c <- res
 		return
 	}
@@ -29,7 +34,7 @@ func CheckLink(link string, c chan string, i int) {
 	// tmp := <-c
 	// fmt.Println(tmp)
 
-	res = string(i) + link + " is up!"
+	res = strconv.Itoa(i) + " -> " + link + " is up!"
 	c <- res
 }
 
@@ -37,6 +42,7 @@ func CheckLinks() {
 	links := GetLink()
 	c := make(chan string)
 
+	println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	for i, link := range links {
 		go CheckLink(link, c, i)
 		// Đặt đây ko khác gì synchronous
@@ -45,9 +51,18 @@ func CheckLinks() {
 
 	// mes := <- c
 	// fmt.Println(mes)
-	fmt.Printf(<-c)
-	println("Done - 1")
-	fmt.Printf(<-c)
-	println("Done - 2")
-	fmt.Printf(<-c)
+	// fmt.Printf(<-c)
+	// println("Done - 1")
+	// fmt.Printf(<-c)
+	// println("Done - 2")
+	// fmt.Printf(<-c)
+
+	for i := 0; i < len(links); i++ {
+		// This log COULD (not always) come up 1st before goroutine init complete
+		// goroutine fire up took some delay
+
+		println("Prepare receiving", i)
+		fmt.Printf(<-c)
+		println("Receive from channel", i)
+	}
 }
